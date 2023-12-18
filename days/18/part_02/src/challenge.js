05cd136ed107a658732c3e8ae7fe8376b937535b
gissehel <public-devgit-orcan@gissehel.org>
2023-12-18 07:47:46 +0100

    Not yet !

--- days/18/part_02/src/challenge.js
+++ days/18/part_02/src/challenge.js
@@ -26,15 +26,52 @@ const is_inside = (x, y, loop) => {
     return result
 }
 
+const has_inside = (y, loop) => {
+    let count = 0
+    const crosses = []
+    for (let index = 0; index < loop.length; index++) {
+        const coord1 = loop[index]
+        const coord2 = loop[(index + 1) % loop.length]
+        if (coord1.y === y && coord2.y === y) {
+            crosses.push({x: coord1.x, exact: true})
+            crosses.push({x: coord2.x, exact: true})
+            // count += Math.abs(coord1.x - coord2.x) - 1
+        } else if (coord1.y < y && coord2.y > y) {
+            crosses.push({x: coord1.x, exact: false})
+        } else if (coord1.y > y && coord2.y < y) {
+            crosses.push({x: coord1.x, exact: false})
+        } else if (coord1.y !== y && coord2.y === y) {
+            crosses.push({x: coord1.x, exact: true})
+        } else if (coord1.y === y && coord2.y !== y) {
+            crosses.push({x: coord1.x, exact: true})
+        }
+        crosses.sort((a, b) => a.x - b.x)
+        
+        while (crosses.length > 1) {
+            let cross1 = crosses.shift()
+            let cross2 = crosses.shift()
+            if (cross1.x === cross2.x) {
+                cross2 = crosses.shift()
+                crosses.shift()
+            }
+            count += Math.abs(cross1.x - cross2.x)
+            if (! cross2.exact) {
+                count += 1
+            }
+        }
+    }
+    return count
+}
+
 const parse_input = (input_lines) => {
     const game = {}
     game.actions = []
     for (let y = 0; y < input_lines.length; y++) {
         const line = input_lines[y];
         const [direction, distance, color] = line.split(' ')
-        const new_distance = parseInt(color.slice(2,7), 16)
-        const new_direction = ({0: 'R', 1: 'D', 2: 'L', 3: 'U'})[color[7]]
-        const action = { direction: new_direction, distance: new_distance}
+        const new_distance = parseInt(color.slice(2, 7), 16)
+        const new_direction = ({ 0: 'R', 1: 'D', 2: 'L', 3: 'U' })[color[7]]
+        const action = { direction: new_direction, distance: new_distance }
         // const action = { direction: direction, distance: parseInt(distance)}
         game.actions.push(action)
     }
@@ -58,20 +95,11 @@ const get_loop = (game) => {
     let min_y = 0
     let max_y = 0
     const loop = []
-    const loop_detail = []
     game.loop = loop
-    game.loop_detail = loop_detail
     for (let index = 0; index < game.actions.length; index++) {
         const action = game.actions[index];
         const direction = directions[action.direction]
         const distance = action.distance
-        let detail_x = x
-        let detail_y = y
-        for (let step = 0; step < distance; step++) {
-            detail_x += direction.x
-            detail_y += direction.y
-            loop_detail.push({ x: detail_x, y: detail_y })
-        }
         x += direction.x * distance
         y += direction.y * distance
         if (x < min_x) min_x = x
@@ -87,19 +115,13 @@ const get_loop = (game) => {
 }
 
 const find_inside = (game) => {
-    const inside = []
-    const loop_keys = Object.fromEntries(game.loop_detail.map(coord => [get_key(coord.x, coord.y), true]))
-    for (let x = game.min_x; x <= game.max_x; x++) {
-        for (let y = game.min_y; y <= game.max_y; y++) {
-            const key = get_key(x, y)
-            if (!loop_keys[key]) {
-                if (is_inside(x, y, game.loop)) {
-                    inside.push({ x, y })
-                }
-            }
-        }
+    let result = 0
+
+    for (let y = game.min_y; y <= game.max_y; y++) {
+        result += has_inside(y, game.loop)
     }
-    game.inside = inside
+
+    return result
 }
 
 /**
@@ -113,8 +135,7 @@ const challenge = (input) => {
     const input_lines = input.split('\n').map(line => line.trim()).filter(line => line.length > 0)
     const game = parse_input(input_lines)
     get_loop(game)
-    find_inside(game)
-    const result = game.loop_detail.length + game.inside.length
+    const result = find_inside(game)
     return `${result}`;
 }
 
